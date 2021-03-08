4700e1196001e9e90804446dce8969ae5c0fd9c4
Doyoon Kim <kim3312@purdue.edu>
2021-03-08 03:37:06 -0500

    Title: Exercise16 Prime Numbers
    This program determine whether input value is prime or not.

---
+++ Exercise/exercise16_PrimeNumbers.py
@@ -0,0 +1,35 @@
+################################################################################
+# Author: Doyoon Kim (kim3312@purdue.edu / doyoon3312@kakao.com)
+# Date: Mar 08, 2021
+# Title: Exercise16 Prime Numbers
+# This program determine whether input value is prime or not.
+################################################################################
+
+# 1 is not a prime number. 2, 3 is a prime number. Among the other values, one that is divided by either 2 or 3 is not
+# a prime number.
+# knownPrimes: list = [1, 2, 3, 5]
+
+def is_prime(num: int):
+    if num == 1:
+        return False
+    for index in range(2, num):
+        if num % index == 0:
+            return False
+    return True
+
+
+def main():
+    while True:
+        integer = int(input("Enter a positive integer (-1 to quit): "))
+        if integer == -1:
+            break
+        else:
+            if is_prime(integer):
+                print(f"{integer} is a prime number.")
+            else:
+                print(f"{integer} is not a prime number.")
+
+
+
+if __name__ == '__main__':
+    main()
