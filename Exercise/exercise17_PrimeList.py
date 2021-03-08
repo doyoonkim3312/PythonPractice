e229c6a86a8c6c78be139c676a4d518a042d03f2
Doyoon Kim <kim3312@purdue.edu>
2021-03-08 03:37:52 -0500

    Title: Exercise17 Prime List
    This program determine, and return all the prime numbers in specific range.

---
+++ Exercise/exercise17_PrimeList.py
@@ -0,0 +1,34 @@
+################################################################################
+# Author: Doyoon Kim (kim3312@purdue.edu / doyoon3312@kakao.com)
+# Date: Mar 08, 2021
+# Title: Exercise17 Prime List
+# This program determine, and return all the prime numbers in specific range.
+################################################################################
+
+# This code will reuse is_prime function in exercise16_PrimeNumber.py.
+
+def is_prime(num: int):
+    if num == 1:
+        return False
+    for index in range(2, num):
+        if num % index == 0:
+            return False
+    return True
+
+
+def main():
+    bound = int(input("Enter a positive integer: "))
+    outputString: str = ""
+    for number in range(2, bound + 1):
+        if is_prime(number) and number == 2:
+            outputString = outputString + f" {number}"
+        elif is_prime(number) and number != 2:
+            outputString = outputString + f", {number}"
+        else:
+            continue
+
+    print(f"The primes up to {bound} are:{outputString}")
+
+
+if __name__ == '__main__':
+    main()
