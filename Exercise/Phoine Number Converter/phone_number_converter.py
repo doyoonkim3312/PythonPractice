fd0b02ef882d6f06ce0d284567d960b114ed3f4f
Doyoon Kim <kim3312@purdue.edu>
2021-04-03 23:52:19 -0400

    Exercise 31 Phone Number Converter
    * [REFACTOR] Modify conditional statement in order to complete gradescope autograder cases.

--- Exercise/Phoine Number Converter/phone_number_converter.py
+++ Exercise/Phoine Number Converter/phone_number_converter.py
@@ -5,7 +5,8 @@
 # those alphabets to their original assigned numeric values.
 ################################################################################
 
-def conver_number(number: str):
+# convert_number: This function takes string type telephone number with alphabets and reverts alphabets to numeric value
+def convert_number(number: str):
     numberOutput: list = list()
     convertedValue: str = ""
     for element in number.split("-"):
@@ -14,23 +15,33 @@ def conver_number(number: str):
             numberOutput.append(element)
         except ValueError:
             for index in range(len(element)):
-                if element[index].upper() == 'A' or element[index].upper() == 'B' or element[index].upper() == 'C':
-                    convertedValue = convertedValue + "2"
-                elif element[index].upper() == 'D' or element[index].upper() == 'E' or element[index].upper() == 'F':
-                    convertedValue = convertedValue + "3"
-                elif element[index] == 'G' or element[index].upper() == 'H' or element[index].upper() == 'I':
-                    convertedValue = convertedValue + "4"
-                elif element[index].upper() == 'J' or element[index].upper() == 'K' or element[index].upper() == 'L':
-                    convertedValue = convertedValue + "5"
-                elif element[index].upper() == 'M' or element[index].upper() == 'N' or element[index].upper() == 'O':
-                    convertedValue = convertedValue + "6"
-                elif element[index].upper() == 'P' or element[index].upper() == 'Q' or element[index].upper() == 'R' or\
-                        element[index].upper() == 'S':
-                    convertedValue = convertedValue + "7"
-                elif element[index].upper() == 'T' or element[index].upper() == 'U' or element[index].upper() == 'V':
-                    convertedValue = convertedValue + "8"
-                else:
-                    convertedValue = convertedValue + "9"
+                try:
+                    int(element[index])
+                    convertedValue = convertedValue + str(element[index])
+                except ValueError:
+                    if element[index].upper() == 'A' or element[index].upper() == 'B' or element[index].upper() == 'C':
+                        convertedValue = convertedValue + "2"
+                    elif element[index].upper() == 'D' or element[index].upper() == 'E' or element[
+                        index].upper() == 'F':
+                        convertedValue = convertedValue + "3"
+                    elif element[index].upper() == 'G' or element[index].upper() == 'H' or element[
+                        index].upper() == 'I':
+                        convertedValue = convertedValue + "4"
+                    elif element[index].upper() == 'J' or element[index].upper() == 'K' or element[
+                        index].upper() == 'L':
+                        convertedValue = convertedValue + "5"
+                    elif element[index].upper() == 'M' or element[index].upper() == 'N' or element[
+                        index].upper() == 'O':
+                        convertedValue = convertedValue + "6"
+                    elif element[index].upper() == 'P' or element[index].upper() == 'Q' or element[
+                        index].upper() == 'R' or \
+                            element[index].upper() == 'S':
+                        convertedValue = convertedValue + "7"
+                    elif element[index].upper() == 'T' or element[index].upper() == 'U' or element[
+                        index].upper() == 'V':
+                        convertedValue = convertedValue + "8"
+                    else:
+                        convertedValue = convertedValue + "9"
             numberOutput.append(convertedValue)
             convertedValue = ""
     return "-".join(numberOutput)
@@ -39,7 +50,7 @@ def conver_number(number: str):
 
 def main():
     userInput = input("Enter a telephone number: ")
-    print(f"The phone number is {conver_number(userInput)}")
+    print(f"The phone number is {convert_number(userInput)}")
 
 
 
