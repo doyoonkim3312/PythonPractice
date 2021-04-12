1e3dbf732f13f2277d2eba16141342fd9ee412c4
Doyoon Kim <kim3312@purdue.edu>
2021-04-12 03:11:13 -0400

    Exercise 37 World Series
    * [REFACTOR] Minor change for gradescope test case

--- Exercise/World Series/world_series.py
+++ Exercise/World Series/world_series.py
@@ -9,47 +9,55 @@ def load_winners_data():
     inputFile = open('WorldSeriesWinners.txt', 'r')
     rawList = inputFile.read().split("\n")
 
-    teamWinningYear = {}
-    year = 1903
-    for team in rawList:
-        if year == 1904 or year == 1994:
-            teamWinningYear[year] = "not played"
-        elif team == "":
-            break
-        else:
-            teamWinningYear[year] = team
-        year = year + 1
+    outputDict = {}
 
-    rawList.sort()
-    rawList.pop(0)
-    winningDict = {}
+    sortedList = rawList.copy()
+    sortedList.sort()
+    sortedList.pop(0)
+    # winningDict = {}
     winningCount = 0
     currentTeam = ""
-    for team in rawList:
+    for team in sortedList:
         if winningCount == 0:
             currentTeam = team
             winningCount = winningCount + 1
         else:
             if currentTeam != team:
-                winningDict[currentTeam] = winningCount
+                outputDict[currentTeam] = winningCount
                 currentTeam = team
                 winningCount = 1
             else:
                 winningCount = winningCount + 1
 
-    return (teamWinningYear, winningDict)
+    # teamWinningYear = {}
+    listIndex = 0
+    year = 1903
+    while year < 2021:
+        if year == 1904 or year == 1994:
+            outputDict[year] = "not played"
+            year = year + 1
+        elif rawList[listIndex] == "":
+            break
+        else:
+            outputDict[year] = rawList[listIndex]
+            year = year + 1
+            listIndex = listIndex + 1
+
+    return outputDict
 
 
 def main():
-    teamWinningByYear, teamWinningCount = load_winners_data()
-
+    # teamWinningByYear, teamWinningCount = load_winners_data()
+    dataCollected = load_winners_data()
     userInput = int(input("Enter a year in the range 1903 -- 2020: "))
-    if userInput == 1904 or userInput == 1994:
-        print(f"The World Series wasn't played in the year {userInput}")
+    if 1903 <= userInput <= 2020:
+        if userInput == 1904 or userInput == 1994:
+            print(f"The World Series wasn't played in the year {userInput}.")
+        else:
+            print(f"The {dataCollected.get(userInput)} won the World Series in {userInput}.")
+            print(f"They have won the World Series {dataCollected.get(dataCollected.get(userInput))} times.")
     else:
-        print(f"The {teamWinningByYear.get(userInput)} won the World Series in {userInput}")
-        print(f"They have won the World Series {teamWinningCount.get(teamWinningByYear.get(userInput))}")
-
+        print(f"Data for the year {userInput} is not included in this system.")
 
 if __name__ == '__main__':
     main()
